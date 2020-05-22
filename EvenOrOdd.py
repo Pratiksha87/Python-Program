266f9270355f0d9739e9682306431cf7d4c80ac9
Pratiksha87 <60502153+Pratiksha87@users.noreply.github.com>
2020-05-22 14:21:14 +0530

    Check number is even or odd python program.

---
+++ EvenOrOdd.py
@@ -0,0 +1,9 @@
+''' Write a program which contains one function named as ChkNum() which accept one parameter as number. If number is even then it should display “Even number” otherwise display “Odd number” on console. '''
+
+def Chk(Num1):
+    if Num1%2==0:
+        print("Even number")
+    else:
+        print("Odd number")
+Num2=4
+Chk(Num2)
