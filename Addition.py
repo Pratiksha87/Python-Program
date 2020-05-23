70ec8e0415a19980bc578c1a08bff14ba0558841
Pratiksha87 <60502153+Pratiksha87@users.noreply.github.com>
2020-05-23 18:59:01 +0530

    Accept two integer number from user and add it.

---
+++ Addition.py
@@ -0,0 +1,9 @@
+''' Write a program which contains one function named as Add() which accepts two numbers from user and return addition of that two numbers'''
+
+
+def Add(num1, num2):
+    ret= int(num1)+int(num2)
+    print(ret)
+Num1 =input()
+Num2=input()
+Add(Num1,Num2)
