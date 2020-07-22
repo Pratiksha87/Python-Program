10c90165deaccff8ed868fc6ae59a2992d98bd2a
Pratiksha87 <60502153+Pratiksha87@users.noreply.github.com>
2020-07-22 13:25:18 +0530

    Find the length of String.

---
+++ Length_Str.py
@@ -0,0 +1,8 @@
+''' Write a program which accept name from user and display length of its name. '''
+string=input("Enter string: ")
+count=0
+for i in string:
+      count=count+1
+print('length: ')
+print(count)
+
